7521e730439c22586fea15b243fdb1c3c5abb3ff
dtran <dtran13899@gmail.com>
2021-12-27 12:44:55 -0600

    Clean some codes

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '72866ac2-93cc-44ea-a808-708777bebff6';
+export const KEY = '56291822-2288-4a22-a013-f945d9b150cf';
 export const MODAL_CLOSE_SEC = 1.5;
